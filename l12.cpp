f31354ad5901b5ab59cdb7e50464132aea204f34
lohith-pixel <slohithreddy15@gmail.com>
2025-05-30 13:14:30 +0530

    Add files via upload

---
+++ l12.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int num;
+    cout << "enter a number: ";
+    cin >> num;
+
+    try
+    {
+         if (num < 0)
+            throw "negative number entered:";
+        cout << "you entered: " << num << endl;
+    }
+    catch(const char *asg)
+    {
+        cout << "exception: " << asg << endl;
+    }
+    return 0;
+}
+    
